efb7a02dc41fd9288ac1872fb8bd0bac28511b45
Hirokazu Yokoyama <hiroyky.voyager@gmail.com>
2018-05-14 01:22:17 +0900

    画像の切り替えを実装1

---
+++ src/components/ImageCarousel.vue
@@ -0,0 +1,35 @@
+<template src="./image-carousel.vue.html"></template>
+
+<script lang="ts">
+import Vue from "vue";
+import {Component, Prop} from "vue-property-decorator";
+
+@Component({})
+export default class extends Vue {
+    @Prop()
+    images: string;
+
+    currentIndex = 0;
+
+    get imageList(): string[] {
+        return JSON.parse(this.images);
+    }
+
+    get imageRange(): number [] {
+        return Array.from({length:this.imageList.length}, (k, v) => v);
+    }
+
+    onPreviousClick() {
+        if(this.currentIndex > 0) {
+            --this.currentIndex;
+        }
+    }
+
+    onNextClick() {
+        if(this.currentIndex < this.imageRange.length - 1) {
+            ++this.currentIndex;
+        }
+    }
+}
+
+</script>
